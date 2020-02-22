fa33b45d09756e72dcbd99674cb54e4c18a6a5e7
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 11:27:25 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,15 @@
 def hello_t(array)
   if block_given?
-  i = 0   
+    i = 0   
   
-  while i < array.length 
-    yield array[i]
-    i = i + 1 
+    while i < array.length 
+      yield array[i]
+      i = i + 1 
+    end
+    array = ["Tim", "Tom", "Jim"]
+  else
+    
   end
-  array = ["Tim", "Tom", "Jim"]
-end
   
 
 
