44248f15b91b936cc924b50c04f23e1b8a2c04e6
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 13:07:30 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,7 +3,7 @@ def hello_t(array)
   if block_given?
     i = 0
     while i < array.length 
-      yield (array[i])
+      yield (array)
       i = i + 1 
     end
   
@@ -16,7 +16,7 @@ def hello_t(array)
 
 
 # hello_t(["Tom","Jim","Tim","Tracy", "Talula"]) do |name|
-hello_t("#{x}") do |name|
+hello_t(array) do |name|
   if name.start_with? ("T")
     puts "Hi, #{name}"
   end
