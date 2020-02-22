0b5194136f6717c873beb5535ea3330e3e5d4088
Gilberto Ocampo <ocampo@gate.net>
2020-02-22 13:18:51 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -17,7 +17,7 @@ def hello_t(array)
 
 # hello_t(["Tom","Jim","Tim","Tracy", "Talula"]) do |name|
 
-hello_t array(x) do |name|
+hello_t array do |name|
   if name.start_with? ("T")
     puts "Hi, #{name}"
   end
